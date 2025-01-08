540eb25773baca028eabe3bc44519ff69cd16ec7
Zhengming Ding <allanzmding@gmail.com>
2025-01-08 16:44:41 +0000

    update Jan8

--- test_main.py
+++ test_main.py
@@ -1,10 +1,13 @@
 from main import *
 
+
 def test_one():
 	""" done. """
 	assert myfunction('Hello') == 'Hello'
-	assert myfunction(1) == 1
-	assert myfunction(6) == -1
+	assert myfunction(11) == 11
+	# assert myfunction(6) == -1
+
 
 def test_two():
-	assert myfunction(3+4) == 7
+	assert myfunction(3 + 4) == 7
+	assert myfunction(9 + 4) == 13
